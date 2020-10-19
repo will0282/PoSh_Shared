b72b1c41e09226c296773effc2eb08e1a653f5e4
will0282 <59141560+will0282@users.noreply.github.com>
2020-10-18 20:43:24 -0400

    Update Netmiko_Automatic_Configuration_Backup.py

--- Netmiko_Automatic_Configuration_Backup.py
+++ Netmiko_Automatic_Configuration_Backup.py
@@ -8,14 +8,14 @@ from datetime import datetime
 print('Start of the script ! ! !')
 #-------------------------------------------
 # Create a dictionary representing the device
-USCG_Test = { 'host': 'FGT60E',
+BREADCRUMB_Test = { 'host': 'FGTXXXXX',
           'device_type': 'fortinet',
           'ip': '192.168.1.99',
-          'username': 'admin',
-          'password': 'admin'}
+          'username': 'admin', 
+          'password': 'admin'} #THIS IS ONLY FOR TESTING. USE CORRECT USERNAME/PASSWORD.
 
 # Establish an SSH connection to the device by passing in the device dictionary
-net_connect = ConnectHandler(**USCG_Test)
+net_connect = ConnectHandler(**BREADCRUMB_Test)
 output = net_connect.send_command('show full-configuration')
 
 #Variable for Date
@@ -26,7 +26,7 @@ current_time = now.strftime("(Time_%H_%M_%S)")
 
 #Creates the file with the format: Hostname_CurrentDate_CurrentTime.cfg
 #File is then save to the save directory as the script.
-with open(str('C:\\Users\\User01\\Desktop\\FGT60E_Backups\\' + USCG_Test['host']) + '_' + str(current_date) + '_' + str(current_time) + '.cfg', 'w') as f:
+with open(str('C:\\Users\\User01\\Desktop\\FGT_Backups\\' + BREADCRUMB_Test['host']) + '_' + str(current_date) + '_' + str(current_time) + '.cfg', 'w') as f:
           for line in output:
               f.write(line)
 
